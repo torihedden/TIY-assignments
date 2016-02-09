324a48ef574b30e2adb77a2888093b30ea6233f0
Tori Hedden <tori.hedden@gmail.com>
2016-02-09 10:42:13 -0500

    Adds verbing exercise

---
+++ verbing.js
@@ -0,0 +1,15 @@
+function verbing(yourString){
+    if (yourString[yourString.length-1] === "g"){
+            console.log(yourString + "ly")
+    } else if (yourString.length >= 3){
+        console.log(yourString + "ing")
+    } else
+        console.log(yourString);
+}
+
+//verbing("mix"); --> "mixing"
+
+verbing("go");
+verbing("throw");
+verbing("a b g");
+verbing("ug");
