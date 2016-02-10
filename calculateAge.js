d58045c12573ca60beabb2cb0c2f549900195395
Tori Hedden <tori.hedden@gmail.com>
2016-02-10 09:53:12 -0500

    Adds calculate age exercise

---
+++ calculateAge.js
@@ -0,0 +1,15 @@
+var firstAge = 0;
+var secondAge = 0;
+
+function calculateAge(birthYear, currentYear){
+   return(currentYear - birthYear);
+}
+
+
+
+// var today = new Date();
+// console.log(today)
+
+console.log(calculateAge(1990,2016));
+console.log(calculateAge(1987,2016));
+console.log(calculateAge(1965,2016));
