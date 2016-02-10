6f0f49407570072097b9b9453196080ab5af797e
Tori Hedden <tori.hedden@gmail.com>
2016-02-10 09:57:33 -0500

    finishes calc age exercise

--- calculateAge.js
+++ calculateAge.js
@@ -2,14 +2,15 @@ var firstAge = 0;
 var secondAge = 0;
 
 function calculateAge(birthYear, currentYear){
-   return(currentYear - birthYear);
+   var yourAge = (currentYear - birthYear)-1;
+   console.log("You are either " + yourAge + " or " + (yourAge+1) + ".")
 }
 
 
-
+//Based on today's date? Which age you would be. This needs your birthDAY, not birth YEAR, though.
 // var today = new Date();
 // console.log(today)
 
-console.log(calculateAge(1990,2016));
-console.log(calculateAge(1987,2016));
-console.log(calculateAge(1965,2016));
+calculateAge(1990,2016); /*--> "You are either 25 or 26."*/
+calculateAge(1987,2016);
+calculateAge(1965,2016);
