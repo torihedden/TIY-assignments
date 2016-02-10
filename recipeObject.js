c6ab83552d670d8162a9415b96040080df364a9c
Tori Hedden <tori.hedden@gmail.com>
2016-02-10 10:39:04 -0500

    Adds recipe object exercise

---
+++ recipeObject.js
@@ -0,0 +1,9 @@
+var fetaHoneyBread = {
+    title: "Honey and Feta Bread",
+    servings: 4,
+    ingredients: ["flour", "honey", "feta cheese", "a little water", "a bit more honey", "butter"]
+}
+
+console.log(fetaHoneyBread.title);
+console.log("Serves: " + fetaHoneyBread.servings);
+console.log("Ingredients:" + fetaHoneyBread.ingredients[0] + fetaHoneyBread.ingredients[1] + fetaHoneyBread.ingredients[2] + fetaHoneyBread.ingredients[3] + fetaHoneyBread.ingredients[4]+ fetaHoneyBread.ingredients[5]);
