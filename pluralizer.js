42cd3b960b4d61e612c00d9ec4d28cb375ec8974
Tori Hedden <tori.hedden@gmail.com>
2016-02-16 10:44:05 -0500

    second commit

--- pluralizer.js
+++ pluralizer.js
@@ -1,8 +1,21 @@
 pluralizer = function(noun, number){
     if (number === 1){
-        console.log(number + noun);
+        console.log(number + " " + noun);
+    } else if (noun[noun.length-1] === "s"){
+        console.log("This word is already plural.");
+    } else if (noun === "sheep" || noun === "geese"){
+        console.log(number + " " + noun);
+    }
+
+    else {
+        console.log(number + " " + noun + "s")    ;
     }
 };
 
 
-console.log(pluralizer("fish", 3));
+pluralizer("fish", 1);
+pluralizer("cat", 5);
+pluralizer("books", 25);
+pluralizer("geese", 50);
+pluralizer("sheep", 50);
+pluralizer("class", 2); //need to write case for ends in "s" but not plural
