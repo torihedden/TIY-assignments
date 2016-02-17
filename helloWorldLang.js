9eff84cb866f54e58a3bc0587d3abe9722ce858f
Tori Hedden <tori.hedden@gmail.com>
2016-02-17 10:31:17 -0500

    Final changes

--- helloWorldLang.js
+++ helloWorldLang.js
@@ -1,11 +1,19 @@
 helloWorld = function(lang){
   if (lang === "es"){
-      console.log("Hola Mundo!");
+      console.log("Hola, Mundo!");
   } else if (lang === "de"){
       console.log("Hallo, Welt!");
+  } else if (lang === "fr"){
+      console.log("Bonjour, le monde!");
+  } else if (lang === "en"){
+      console.log("Hello, world!");
   } else {
       console.log("Hello, world!");
   }
 };
 
-helloWorld("de");
+helloWorld(""); // => "Hello, world!"
+helloWorld("de"); // => "Hallo, Welt!"
+helloWorld("en"); // => "Hello, world!"
+helloWorld("fr"); // => "Bonjour, le monde!"
+helloWorld("es"); // => "Hola, Mundo!"
