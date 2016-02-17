e187dbba3e85f2341ab6b9c4529ce610ecfe9884
Tori Hedden <tori.hedden@gmail.com>
2016-02-17 10:24:56 -0500

    Initial commit

---
+++ helloWorldLang.js
@@ -0,0 +1,11 @@
+helloWorld = function(lang){
+  if (lang === "es"){
+      console.log("Hola Mundo!");
+  } else if (lang === "de"){
+      console.log("Hallo, Welt!");
+  } else {
+      console.log("Hello, world!");
+  }
+};
+
+helloWorld("de");
