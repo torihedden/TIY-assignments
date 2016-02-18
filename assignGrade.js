69ccb78a0221e9b5a23d71e94cec8f861dd9f075
Tori Hedden <tori.hedden@gmail.com>
2016-02-18 10:36:49 -0500

    Final commit

--- assignGrade.js
+++ assignGrade.js
@@ -1,7 +1,7 @@
 assignGrade = function(score){
     if (score <60){
         console.log("You got an F.");
-    } else if (score < 70 && score >= 60){
+    } else if (score >= 60 && score < 70){
         console.log("You got a D.");
     } else if (score >= 70 && score < 80){
         console.log("You got a C.");
@@ -17,6 +17,11 @@ assignGrade = function(score){
 
 };
 
-assignGrade(72);
+assignGrade(55); => You got an F
+assignGrade(62);
+assignGrade(71);
 assignGrade(101);
 assignGrade(82);
+assignGrade(99);
+assignGrade(100);
+assignGrade("A");
