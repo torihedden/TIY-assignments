a454972d17b4cedd9b936e62a8acf747f85abbbf
Tori Hedden <tori.hedden@gmail.com>
2016-02-22 10:47:58 -0500

    Adds case if something that is NaN is passed to the fxn

--- greaterNumber.js
+++ greaterNumber.js
@@ -1,5 +1,7 @@
 greaterNum = function(num1, num2){
-  if (num1 === num2){
+  if (isNaN(num1) || isNaN(num2)){
+      console.log("those don't look like numbers...");
+  } else if (num1 === num2){
       console.log("Those numbers are the same.");
   } else if (num1 > num2){
       console.log(num1 + " is greater.");
@@ -11,3 +13,5 @@ greaterNum = function(num1, num2){
 greaterNum(0, 19);
 greaterNum(2,2);
 greaterNum(100, 5);
+greaterNum(-5, 5);
+greaterNum("cats", 3);
