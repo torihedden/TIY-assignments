f235958dc80a21adb2aa8b2406dad53a423ad12a
Tori Hedden <tori.hedden@gmail.com>
2016-02-25 10:40:01 -0500

    Final changes to choices array

--- choicesArray.js
+++ choicesArray.js
@@ -1,19 +1,22 @@
 var animalChoices = ["cats", "kittens", "puppies", "dogs", "hedgehogs"];
 
-var foodChoices = ["sushi", "candy", "Indian food"];
+var foodChoices = ["sushi", "candy", "Indian food", "tacos"];
 
-var myChoices = function(array, ranking){
+var myChoices = function(array, rank){
     var rankFormat;
-    var rank = ranking-1;
     if (rank === 1){
         rankFormat = rank + "st";
-    } else if (rank === 2) {
+    } else if (rank === 2){
         rankFormat = rank + "nd";
+    } else if (rank === 3){
+        rankFormat = rank + "rd";
+    } else if (rank === 4){
+        rankFormat = rank + "th";
     }
 
-    console.log("My " + rankFormat + " choice is " + array[ranking-1] + ".");
+    console.log("My " + rankFormat + " choice is " + array[rank-1] + ".");
 };
 
-myChoices(animalChoices, 3);
-
-myChoices(foodChoices, 2);
+myChoices(animalChoices, 3); // => "My 3rd choice is puppies."
+myChoices(foodChoices, 2); // => "My 2nd choice is candy."
+myChoices(animalChoices, 4);// => "My 4th choice is dogs."
