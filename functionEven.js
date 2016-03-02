b7e2fced02c2fc506e177bd26d42c8a550b6883c
Tori Hedden <tori.hedden@gmail.com>
2016-03-02 10:30:15 -0500

    add functionEven

---
+++ functionEven.js
@@ -0,0 +1,12 @@
+var checkEven = function(number){
+    for (number = 0; number <=21044; number++){
+    if (number % 2 === 0){
+        console.log(number + " is even");
+    } else {
+        console.log(number + " is odd");
+    }
+    }
+};
+
+
+checkEven();
