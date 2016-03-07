552a3b15d3297c258f4a1d9e25066e7e641a5df2
Tori Hedden <tori.hedden@gmail.com>
2016-03-07 10:27:44 -0500

    add letter capitalizer exercise

---
+++ capitalizeLetter.js
@@ -0,0 +1,9 @@
+var LetterCapitalize = function(string){
+
+    string = string[0].toUpperCase() + string.slice(1);
+    console.log(string);
+};
+
+
+LetterCapitalize("cat");
+LetterCapitalize("tori");
