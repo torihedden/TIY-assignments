ab7698d2b3c202863eeab3fdc34cd993e3548b28
Tori Hedden <tori.hedden@gmail.com>
2016-03-07 10:38:38 -0500

    final answer capitalize letter exercise

--- capitalizeLetter.js
+++ capitalizeLetter.js
@@ -1,9 +1,22 @@
-var LetterCapitalize = function(string){
+var finishedString = [];
 
-    string = string[0].toUpperCase() + string.slice(1);
-    console.log(string);
+var LetterCapitalize = function(string){
+    stringSplit = string.split(" ");
+    console.log(stringSplit);
+
+    for (i = 0; i < stringSplit.length; i++){
+        eachString = (stringSplit[i])[0].toUpperCase() + stringSplit[i].slice(1);
+
+        finishedString.push(eachString);
+
+        console.log(eachString);
+
+    }
+
+    var answer = finishedString.join(" ");
+    console.log(answer);
 };
 
 
-LetterCapitalize("cat");
-LetterCapitalize("tori");
+// LetterCapitalize("cat in the hat");
+LetterCapitalize("victoria hedden");
