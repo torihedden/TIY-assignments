8b6b47a2c1bfbbc48687b548c19494ff11c02555
Tori Hedden <tori.hedden@gmail.com>
2016-03-16 10:44:27 -0400

    eliminates console log tests

--- maxLength.js
+++ maxLength.js
@@ -1,27 +1,17 @@
 // Write a JS function that accepts the list of country names as inputs and returns the longest one.
 
-
-var sample = ["USA", "Germany", "India"];
+var sample = ["USA", "Germany", "India", "longest country name"];
 var lengthArray = [];
 
 
 var longerCountry = function(array){
     for (var i = 0; i < array.length; i++){
         lengthArray.push(array[i].length);
-
     }
-    console.log(lengthArray);
 
     var mathMax = Math.max.apply(0, lengthArray);
-
-    console.log(mathMax);
-
     var theIndex = lengthArray.indexOf(mathMax);
-    console.log(theIndex);
-
-
     return sample[theIndex];
 };
 
 longerCountry(sample);
-// => 'Germany'
