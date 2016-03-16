f2878f50f6fc325e00989b883a9954d34c93e88f
Tori Hedden <tori.hedden@gmail.com>
2016-03-16 10:35:11 -0400

    add max word in array exercise

---
+++ maxLength.js
@@ -0,0 +1,23 @@
+var sample = ["USA", "Germany", "India"];
+var lengthArray = [];
+
+
+var longerCountry = function(array){
+    for (var i = 0; i < array.length; i++){
+        lengthArray.push(array[i].length);
+
+    }
+    console.log(lengthArray);
+
+    var mathMax = Math.max.apply(0, lengthArray);
+
+    console.log(mathMax);
+
+    var theIndex = lengthArray.indexOf(mathMax);
+    console.log(theIndex);
+
+
+    return sample[theIndex];
+};
+
+longerCountry(sample);
